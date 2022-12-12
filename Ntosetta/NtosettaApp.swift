b2f4b02ef3de3c0178ddda35d7abce82063ffc61
Pantos, Thomas <tpantos@deloitte.gr>
2022-12-13 01:37:02 +0200

    Initial Commit

---
+++ Ntosetta/NtosettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NtosettaApp.swift
+//  Ntosetta
+//
+//  Created by Pantos, Thomas on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NtosettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
